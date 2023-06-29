21ab05a750892612ce64815c74815a3c034c7625
Thar Lin Shwe <albent2017@gmail.com>
2023-06-29 19:58:42 +0800

    Integrated tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs", "./public/**/*.{html, css,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
