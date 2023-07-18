225080a79b45f1a0f048bd113661b8135224df0d
Thar Lin Shwe <albent2017@gmail.com>
2023-07-18 11:58:57 +0800

    Integrated express-ejs-layouts

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const { connect } = require("./utils/database");
 const { PORT } = require("./config/constant");
 const path = require("path");
 const userRoutes = require("./routes/user");
+const authorRoutes = require("./routes/authors");
 
 const app = express();
 //set the app to use ejs for rendering
 app.set("view engine", "ejs");
-
+app.set("layout", "layouts/index.ejs");
+app.use(expressLayouts);
 // expose the css file from dist folder
 app.use(express.static(path.join(__dirname, "dist")));
 
 // configure routes
 app.use("/user", userRoutes); //this adds all the userRoutes to the app under the path /user
+app.use("/authors", authorRoutes);
 
 module.exports.start = async () => {
   await connect();
