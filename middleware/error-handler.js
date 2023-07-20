338bf061cb6ed516d2bbd3171f05e44d054b03da
Thar Lin Shwe <albent2017@gmail.com>
2023-07-20 12:58:50 +0800

    Implemented error handling middleware

---
+++ middleware/error-handler.js
@@ -0,0 +1,25 @@
+const { StatusCodes } = require("http-status-codes");
+const { CustomApiError } = require("../errors");
+
+async function errorHandler(err, req, res, next) {
+  if (err instanceof CustomApiError) {
+    return res.status(err.status).send({
+      err: {
+        message: err.message,
+      },
+    });
+  }
+
+  const customError = {
+    status: err.status || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Internal server error",
+  };
+
+  return res.status(customError.status).json({
+    err: {
+      mesage: customError.msg,
+    },
+  });
+}
+
+module.exports.errorHandler = errorHandler;
