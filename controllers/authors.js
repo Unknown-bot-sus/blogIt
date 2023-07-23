efea0352647575d28bf5d38f8f453a16869bb258
Thar Lin Shwe <albent2017@gmail.com>
2023-07-23 22:10:07 +0800

    Implemented Author controllers

---
+++ controllers/authors.js
@@ -0,0 +1,73 @@
+const { StatusCodes } = require("http-status-codes");
+
+function homePage(req, res) {
+  db.get(
+    "SELECT Authors.*, Users.* FROM Authors INNER JOIN Users ON  Authors.userId = Users.id WHERE Authors.id = ?",
+    [req.params.id],
+    (err, user) => {
+      db.all(
+        "SELECT * FROM Articles WHERE authorId = ?",
+        [req.params.id],
+        (err, articles) => {
+          const { publishedArticles, unpublishedArticles } = articles.reduce(
+            (accumulator, article) => {
+              if (article.publication_date !== null) {
+                accumulator.publishedArticles.push(article);
+              } else {
+                accumulator.unpublishedArticles.push(article);
+              }
+
+              return accumulator;
+            },
+            {
+              publishedArticles: [],
+              unpublishedArticles: [],
+            }
+          );
+          res.status(StatusCodes.OK).render("authors/index", {
+            title: "Home",
+            user,
+            articles: unpublishedArticles,
+            publishedArticles,
+          });
+        }
+      );
+    }
+  );
+}
+
+function settingsPage(req, res) {
+  db.get(
+    "SELECT Authors.*, Users.* FROM Authors INNER JOIN Users ON  Authors.userId = Users.id WHERE Authors.id = ?",
+    [req.params.id],
+    (err, user) => {
+      res.render("authors/settings", {
+        title: "Settings",
+        user,
+      });
+    }
+  );
+}
+
+function putAuthor(req, res) {
+  const user = req.body;
+  db.get(
+    "UPDATE Authors SET title = ?, subtitle = ? WHERE id = ? RETURNING userId",
+    [user.title, user.subtitle, req.params.id],
+    (err, author) => {
+      db.run(
+        "UPDATE Users SET name = ? WHERE id = ?",
+        [user.name, author.userId],
+        (err) => {
+          res.status(StatusCodes.NO_CONTENT).send();
+        }
+      );
+    }
+  );
+}
+
+module.exports = {
+  homePage,
+  settingsPage,
+  putAuthor,
+};
