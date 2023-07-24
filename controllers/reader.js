c52dada5d1433ca39f38c1e52109afa69fc7967e
Thar Lin Shwe <albent2017@gmail.com>
2023-07-24 12:27:31 +0800

    Implemented Reader homepage

---
+++ controllers/reader.js
@@ -0,0 +1,23 @@
+function homePage(req, res) {
+  db.all("SELECT * FROM Authors", (err, authors) => {
+    db.all(
+      `
+      SELECT Articles.*, IFNULL(Likes.likes, 0) AS likes FROM
+      (SELECT * FROM Articles WHERE publication_date IS NOT NULL) AS Articles LEFT JOIN 
+      (SELECT COUNT(id) as likes, articleId FROM Likes GROUP BY articleId) AS Likes ON
+      Articles.id = Likes.articleId ORDER BY Articles.publication_date DESC
+      `,
+      (err, articles) => {
+        res.render("reader/index", {
+          title: "Reader Page",
+          authors,
+          articles,
+        });
+      }
+    );
+  });
+}
+
+module.exports = {
+  homePage,
+};
