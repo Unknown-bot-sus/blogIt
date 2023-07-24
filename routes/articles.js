2633b556e1bd4f99556e72a5496d85e3d5ffef98
Thar Lin Shwe <albent2017@gmail.com>
2023-07-24 13:01:28 +0800

    Implemented  liking article route

--- routes/articles.js
+++ routes/articles.js
@@ -6,11 +6,14 @@ const {
   editArtcile,
   editPage,
   detailPage,
+  like,
 } = require("../controllers/articles");
 
 router.route("").post(createArticle);
 
 router.get("/:id/edit", editPage);
+
+router.route("/:id/likes").post(like);
 
 router.route("/:id").get(detailPage).put(editArtcile).delete(deleteArticle);
 
